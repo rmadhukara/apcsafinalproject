14ff63889a84fa0c5c299485e91fac72f0c45bc7
The Traveling Salesman <rmadhu4000@gmail.com>
2019-05-29 21:53:52 -0700

    run method

--- Player.java
+++ Player.java
@@ -66,8 +66,81 @@ public class Player implements Playerable, Runnable
     controlCount = x;
   }
 
-  public void run()
+  //Override from Runnable
+  public void run() 
   {
     try 
+    {
+      setup();
+      processCommands();
+    } 
+    catch (Exception e) 
+    {
+      e.printStackTrace();
+    } 
+    finally 
+    {
+      if (opponent != null && opponent.output != null) 
+      {
+          opponent.output.println("OTHER_PLAYER_LEFT");
+      }
+      try 
+      {
+        socket.close();
+      } 
+      catch (IOException e) {}
+    }
+  }
+
+  private void setup() throws IOException 
+  {
+    input = new Scanner(socket.getInputStream());
+    output = new PrintWriter(socket.getOutputStream(), true);
+    output.println("WELCOME " + mark);
+    if (mark == 'X') {
+        currentPlayer = this;
+        output.println("MESSAGE Waiting for opponent to connect");
+    } else {
+        opponent = currentPlayer;
+        opponent.opponent = this;
+        opponent.output.println("MESSAGE Your move");
+    }
+  }
+
+  private void processCommands() 
+  {
+    while (input.hasNextLine()) 
+    {
+      String command = input.nextLine();
+      if (command.startsWith("QUIT")) {
+          return;
+      } else if (command.startsWith("MOVE")) {
+          processMoveCommand(Integer.parseInt(command.substring(5)));
+      }
+    }
+  }
+
+  private void processMoveCommand(int location) 
+  {
+    try 
+    {
+      move(location, this);
+      output.println("VALID_MOVE");
+      opponent.output.println("OPPONENT_MOVED " + location);
+      if (hasWinner()) 
+      {
+        output.println("VICTORY");
+        opponent.output.println("DEFEAT");
+      } 
+      else if (boardFilledUp()) 
+      {
+        output.println("TIE");
+        opponent.output.println("TIE");
+      }
+    } 
+    catch (IllegalStateException e) 
+    {
+      output.println("MESSAGE " + e.getMessage());
+    }
   }
 }
