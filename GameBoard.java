75cd9a15df617c1216c162e4adc4d7b5e16e49cd
Kaila Coimbra <hal9000@kailas-mbp.sduhsd.lan>
2019-05-31 14:39:46 -0700

    changed size

--- GameBoard.java
+++ GameBoard.java
@@ -34,7 +34,7 @@ public class GameBoard extends Grid
 	}
 
 	public void paint(Graphics window){
-		box.setSize(7);
+		box.setSize(15);
 		box.draw(window, 10, 300, Color.RED);
 		box.draw(window, 20, 300, Color.GREEN);
 		box.draw(window, 30, 300, Color.YELLOW);
--- Grid.java
+++ Grid.java
@@ -17,13 +17,13 @@ public class Grid extends Canvas
 		grid = new Block[8][7];
                 for(int h = 0; h < 7; h++){
                         for(int w = 0; w < 8; w++){
-                                grid[w][h] = new Block(55,0);
+                                grid[w][h] = new Block(15,0);
                         }
                 }
 
 		//setting corner blocks to Player 1 and 2 blocks
-		grid[7][0] = new Block(5,1);
-		grid[0][6] = new Block(5,2);
+		grid[7][0] = new Block(15,1);
+		grid[0][6] = new Block(15,2);
 
 	}
 
@@ -42,11 +42,11 @@ public class Grid extends Canvas
 		for(int row = 0; row < 8; row++){
 			for(int col = 0; col < 7; col++){
 				Block one = grid[row][col];
-				one.draw(window,270-(5*row),10+(5*col),one.getColor());
+				one.draw(window,270-(15*row),10+(15*col),one.getColor());
 			}
 		}
-		window.drawString("1", 266, 12);
-		window.drawString("2", 11, 42);
+		window.drawString("1", 12, 266);
+		window.drawString("2", 42, 11);
 
 	}
 
