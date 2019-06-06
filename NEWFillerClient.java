d330d2ad9527b23ef003f5d22924e93bb7249d3a
The Traveling Salesman <rmadhu4000@gmail.com>
2019-06-06 08:33:17 -0700

    minor edits

--- NEWFillerClient.java
+++ NEWFillerClient.java
@@ -156,6 +156,10 @@ public class NEWFillerClient {
                 }
 
                 board.setStatusInGrid(statusBoardInts);
+
+                // frame.getContentPane().repaint();
+                
+
               }
           }
           out.println("QUIT");
