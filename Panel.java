5475a4e57e7f71d7189e6e08e340689d8be3e959
Katie Hsieh <katiehsieh25@gmail.com>
2019-06-10 22:50:47 -0700

    clean

--- Panel.java
+++ Panel.java
@@ -7,17 +7,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panel extends JPanel {
-  JLabel label = new JLabel();
-
+  
   public Panel(Color col) {
       setBackground(col);
       setLayout(new GridBagLayout());
-      label.setFont(new Font("Arial", Font.BOLD, 40));
-      add(label);
-  }
-
-  public void setText(char text) {
-      label.setForeground(text == 'X' ? Color.BLUE : Color.RED);
-      label.setText(text + "");
   }
 }
